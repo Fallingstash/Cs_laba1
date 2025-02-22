5456082b056bf6055aa0d60814fe1d8857306bde
Fallingstash <sashkaprog228@gmail.com>
2025-02-22 17:20:33 +0700

    коммит 1
    поменял нейминг переменных и отделил логические блоки пробелом.

--- Program.cs
+++ Program.cs
@@ -10,50 +10,56 @@ namespace ConsoleApp7
   {
     static void Main(string[] args) {
       Console.WriteLine("Выберите действие. \nНажмите 1 для того чтобы в введенном числе зачеркнуть вторую цифру и добавить её в конец\nНажмите 2 для того чтобы возвести число в степень");
-      string message = Console.ReadLine();
-      switch (message)
+      string userResponse = Console.ReadLine();
+      switch (userResponse)
       {
         case "1":
           Console.WriteLine("Введите число, в котором вы хотите зачеркнуть вторую цифру");
           int firstNumber = Convert.ToInt32(Console.ReadLine());
-          int test = firstNumber;
-          int count1 = 0;
-          while (test > 0)
+          int quantityCheck = firstNumber;
+          int countDigitals = 0;
+
+          while (quantityCheck > 0)
           {
-            test /= 10;
-            count1++;
+            quantityCheck /= 10;
+            countDigitals++;
           }
-          int eraser = 1;
+
+          int placeErasedNumber = 1;
           string result1 = "";
-          for (int count = 0; count < --count1; ++count)
+
+          for (int count = 0; count < --countDigitals; ++count)
           {
-            eraser *= 10;
+            placeErasedNumber *= 10;
           }
-          string erasedDigital = Convert.ToString((firstNumber * 10 / eraser) % 10);
-          for (int i = 0; i < count1; ++i)
+          string erasedDigital = Convert.ToString((firstNumber * 10 / placeErasedNumber) % 10);
+
+          for (int i = 0; i < countDigitals; ++i)
           {
-            if (i != count1 - 2)
+            if (i != countDigitals - 2)
               result1 = Convert.ToString(firstNumber % 10) + result1;
             firstNumber /= 10;
           }
           result1 += erasedDigital;
+
           Console.WriteLine($"Великолепно! Вот ваше число: {result1}");
           break;
+
         case "2":
           Console.WriteLine("Введите основание степени");
           int value = Convert.ToInt32(Console.ReadLine());
           Console.WriteLine("Введите показатель степени");
           int cycles = Convert.ToInt32(Console.ReadLine());
+
           int result = 1;
           for (int count = 0; count < cycles; ++count)
           {
             result *= value;
           }
+
           Console.WriteLine($"Ваш результат: {result}");
           break;
       }
-
-
     }
   }
 }
