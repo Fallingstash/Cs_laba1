9c7fe8e55c380610413afe296a0eeb7592631da9
Fallingstash <sashkaprog228@gmail.com>
2025-02-22 18:56:39 +0700

    добавил методы для решения заданий.

--- Program.cs
+++ Program.cs
@@ -1,65 +1,84 @@
 ﻿using System;
+using static System.Console;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+
+
 namespace ConsoleApp7
 {
-  class Program
-  {
-    static void Main(string[] args) {
-      Console.WriteLine("Выберите действие. \nНажмите 1 для того чтобы в введенном числе зачеркнуть вторую цифру и добавить её в конец\nНажмите 2 для того чтобы возвести число в степень");
-      string userResponse = Console.ReadLine();
-      switch (userResponse)
-      {
-        case "1":
-          Console.WriteLine("Введите число, в котором вы хотите зачеркнуть вторую цифру");
-          int firstNumber = Convert.ToInt32(Console.ReadLine());
-          int quantityCheck = firstNumber;
-          int countDigitals = 0;
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Выберите действие. \nНажмите 1 для того чтобы в введенном числе зачеркнуть вторую цифру и добавить её в конец\nНажмите 2 для того чтобы возвести число в степень");
+            string userResponse = Console.ReadLine();
+            switch (userResponse)
+            {
+                case "1":
+                    Console.WriteLine("Введите число, в котором вы хотите зачеркнуть вторую цифру");
+                    int value = Convert.ToInt32(Console.ReadLine());
+                    string result = EraseAndDrawDigital(value);
+                    Console.WriteLine($"Великолепно! Вот ваше число: {result}");
+                    break;
 
-          while (quantityCheck > 0)
-          {
-            quantityCheck /= 10;
-            countDigitals++;
-          }
+                case "2":
+                    Console.WriteLine("Введите основание степени");
+                    int value1 = Convert.ToInt32(Console.ReadLine());
+                    Console.WriteLine("Введите показатель степени");
+                    int cycles = Convert.ToInt32(Console.ReadLine());
 
-          int placeErasedNumber = 1;
-          string result1 = "";
+                    int result1 = PowBySoldatov(value1, cycles);
+                    Console.WriteLine($"Ваш результат: {result1}");
+                    break;
 
-          for (int count = 0; count < --countDigitals; ++count)
-          {
-            placeErasedNumber *= 10;
-          }
-          string erasedDigital = Convert.ToString((firstNumber * 10 / placeErasedNumber) % 10);
+                default:
+                    Console.WriteLine("Неверный формат. Пожалуйста, выберите 1 или 2");
+                    break;
+            }
+        }
+        static string EraseAndDrawDigital(int firstNumber)
+        {
+            int quantityCheck = firstNumber;
+            int countDigitals = 0;
 
-          for (int i = 0; i < countDigitals; ++i)
-          {
-            if (i != countDigitals - 2)
-              result1 = Convert.ToString(firstNumber % 10) + result1;
-            firstNumber /= 10;
-          }
-          result1 += erasedDigital;
+            while (quantityCheck > 0)
+            {
+                quantityCheck /= 10;
+                countDigitals++;
+            }
 
-          Console.WriteLine($"Великолепно! Вот ваше число: {result1}");
-          break;
+            int placeErasedNumber = 1;
+            string result = "";
 
-        case "2":
-          Console.WriteLine("Введите основание степени");
-          int value = Convert.ToInt32(Console.ReadLine());
-          Console.WriteLine("Введите показатель степени");
-          int cycles = Convert.ToInt32(Console.ReadLine());
+            for (int count = 0; count < countDigitals - 2; ++count)
+            {
+                placeErasedNumber *= 10;
+            }
+            string erasedDigital = Convert.ToString((firstNumber / placeErasedNumber) % 10);
 
-          int result = 1;
-          for (int count = 0; count < cycles; ++count)
-          {
-            result *= value;
-          }
+            for (int i = 0; i < countDigitals; ++i)
+            {
+                if (i != countDigitals - 2)
+                {
+                    result = Convert.ToString(firstNumber % 10) + result;
+                }
+                firstNumber /= 10;
+            }
+            result += erasedDigital;
 
-          Console.WriteLine($"Ваш результат: {result}");
-          break;
-      }
+            return result;
+        }
+        static int PowBySoldatov(int value, int quantity)
+        {
+            int result = 1;
+            for (int count = 0; count < quantity; ++count)
+            {
+                result *= value;
+            }
+            return result;
+        }
     }
-  }
 }
